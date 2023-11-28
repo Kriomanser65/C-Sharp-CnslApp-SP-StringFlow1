6b2edea8d55a8e9007fe4d41d57bf80bcdf5893b
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-28 16:15:17 +0200

    Add project files.

---
+++ stringedsfg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace stringedsfg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+            Thread thread = new Thread(() =>
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            });
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Main flow enden work.");
+        }
+    }
+}
